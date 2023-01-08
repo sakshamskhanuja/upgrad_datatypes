7e2cec08c17a897dd607d25697720d50abcb9b1e
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-08 15:43:21 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float distance = 2.367F;
+        int distanceInMetres = 2367;
+        System.out.println("Distance in km=" + distance);
+        System.out.println("Distance in metres=" + distanceInMetres);
+    }
+}
